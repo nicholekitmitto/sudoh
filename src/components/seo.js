2e49083b613e721e3ef89ba21c1d5ab70788b266
Nichole Dwight <nicholedwight@gmail.com>
2020-10-01 06:04:58 -0700

    commenting error

--- src/components/seo.js
+++ src/components/seo.js
@@ -56,10 +56,10 @@ const SEO = ({ description, lang, meta, title }) => {
           name: `twitter:card`,
           content: `summary`,
         },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.social.twitter,
-        },
+        // {
+        //   name: `twitter:creator`,
+        //   content: site.siteMetadata.social.twitter,
+        // },
         {
           name: `twitter:title`,
           content: title,
